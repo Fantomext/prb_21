7c8cb72ea0eefbea2d33866f422d29c6a21e8e62
Fantomext <96086777+Fantomext@users.noreply.github.com>
2021-12-22 02:46:29 +0300

    Add files via upload

--- Lesson 2/Task 3/task3.py
+++ Lesson 2/Task 3/task3.py
@@ -1,1 +1,20 @@
+nonRecurringCount = 0
+checkRecurring = 0
+
+def NonRecurring(nonRecurringCount,checkRecurring):
+    print("Введите строку")
+    myStr = input()
+
+    for i in range(len(myStr)):
+        checkRecurring = 0
+        for y in range(len(myStr)):
+            if myStr[i] == myStr[y]:
+                checkRecurring +=1
+        if checkRecurring == 1:
+            nonRecurringCount += 1
+    print("неповторяющихся символов", nonRecurringCount) 
+
+        
+
+NonRecurring(nonRecurringCount,checkRecurring)
 
