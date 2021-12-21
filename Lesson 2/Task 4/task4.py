e4231dcbf062574b05eb98629692474e78971bf8
Fantomext <96086777+Fantomext@users.noreply.github.com>
2021-12-22 02:56:33 +0300

    Add files via upload

--- Lesson 2/Task 4/task4.py
+++ Lesson 2/Task 4/task4.py
@@ -1,1 +1,20 @@
+## К сожалению, задачи с сайта я не понял 
 
+kolvoSlot = input()
+numberSlot = input()
+nMinusOne = input()
+
+if (3 <= kolvoSlot <=3*10**4):
+    n=kolvoSlot
+
+if (2 <= numberSlot <= kolvoSlot):
+    t= numberSlot
+
+if (1 <= nMinusOne <=n-i)
+
+
+
+if a == t:
+    print(yes)
+else:
+    print(no)
