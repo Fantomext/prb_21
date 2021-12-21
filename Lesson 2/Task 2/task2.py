59d5107d488a07901970c33bd1ea08af79438388
Fantomext <96086777+Fantomext@users.noreply.github.com>
2021-12-22 02:44:50 +0300

    Add files via upload

--- Lesson 2/Task 2/task2.py
+++ Lesson 2/Task 2/task2.py
@@ -1,1 +1,16 @@
+def PalindromeIs(lengthMyStr, count):
+    count = 0
+    for x in range(lengthMyStr):
+        if myStr[0+x] == myStr[-1-x]:
+            count += 1
+            if count == lengthMyStr:
+                print("Строка является палиндромом")
+            else:
+                print("Строка не является палиндромом")
 
+print("Введите строку")
+myStr = input()
+lengthMyStr = len(myStr)
+count = 0
+
+PalindromeIs(lengthMyStr, count)
